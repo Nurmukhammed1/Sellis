a76d1723bc4df28ddb1a434d947e3c82fd894bc5
Nurmukhammed Kanafin <nurmukhammedkanafin@Nurmukhammeds-MacBook-Air.local>
2025-04-05 19:35:36 +0500

    Add product detail, adding item to cart

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('products', views.products, name='products'),
+    path('products/<slug:slug>', views.product_detail, name='product_detail'),
+    path('add_item', views.add_item, name='add_item')
 ]
 
--- shop_app/views.py
+++ shop_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import *
 
 @api_view(['GET'])
 def products(request):
@@ -10,4 +10,28 @@ def products(request):
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
- 
+
+@api_view(['GET'])
+def product_detail(request, slug):
+    product = Product.objects.get(slug=slug)
+    serializer = DetailedProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def add_item(request):
+    try:
+        cart_code = request.data.get('cart_code')
+        product_id = request.data.get('product_id')
+
+        cart, created = Cart.objects.get_or_create(cart_code=cart_code)
+        product = Product.objects.get(id=product_id)
+
+        cartitem, created = CartItem.objects.get_or_create(cart=cart, product=product)
+        cartitem.quantity = 1
+        cartitem.save()
+
+        serializer = CartItemSerializer(cartitem)
+        return Response({'data': serializer.data, 'message': 'Cartitem created successfully'}, status=201)
+    except Exception as e:
+        return Response({'error': str(e)}, status=400)
