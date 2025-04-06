3e756780a330d49d5d2eb7294821e693cacd6c91
Nurmukhammed Kanafin <nurmukhammedkanafin@Nurmukhammeds-MacBook-Air.local>
2025-04-07 00:56:14 +0500

    Add full cart functionality

--- shop_app/serializers.py
+++ shop_app/serializers.py
@@ -7,7 +7,6 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'slug', 'image', 'description', 'category', 'price']
 
 
-
 class DetailedProductSerializer(serializers.ModelSerializer):
     similar_products = serializers.SerializerMethodField()
     class Meta:
@@ -18,17 +17,55 @@ class DetailedProductSerializer(serializers.ModelSerializer):
         products = Product.objects.filter(category=product.category).exclude(id=product.id)
         serializer = ProductSerializer(products, many=True)
         return serializer.data
-    
-
-class CartSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cart
-        fields = ['id', 'cart_code', 'created_at', 'modified_at']
 
 
 class CartItemSerializer(serializers.ModelSerializer):
     product = ProductSerializer(read_only=True)
-    cart = CartSerializer(read_only=True)
+    total_price = serializers.SerializerMethodField()
+
     class Meta:
         model = CartItem
-        fields = ['id', 'quantity', 'product', 'cart']
+        fields = ['id', 'quantity', 'product', 'total_price']
+
+    def get_total_price(self, cartitem):
+        total_price = cartitem.product.price * cartitem.quantity
+        return total_price
+
+
+class SimpleCartSerializer(serializers.ModelSerializer):
+    num_of_items = serializers.SerializerMethodField()
+    class Meta:
+        model = Cart
+        fields= ['id', 'cart_code', 'num_of_items']
+
+    def get_num_of_items(self, cart):
+        num_of_items = sum([item.quantity for item in cart.items.all()])
+        return num_of_items     
+    
+
+class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(read_only=True, many=True)
+    sum_total = serializers.SerializerMethodField()
+    num_of_items = serializers.SerializerMethodField()
+    class Meta:
+        model = Cart
+        fields = ['id', 'cart_code', 'items', 'sum_total', 'num_of_items', 'created_at', 'modified_at']
+
+    def get_sum_total(self, cart):
+        items = cart.items.all()
+        total = sum([item.product.price * item.quantity for item in items])
+        return total
+    
+    def get_num_of_items(self, cart):
+        items = cart.items.all()
+        num_of_items = sum([item.quantity for item in items])
+        return num_of_items
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    class Meta:
+        model = CartItem
+        fields = ['id', 'quantity', 'product']
+
+
--- shop_app/views.py
+++ shop_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Product
+from rest_framework import status
+from .models import Product, Cart, CartItem
 from .serializers import *
 
 @api_view(['GET'])
@@ -21,8 +22,8 @@ def product_detail(request, slug):
 @api_view(['POST'])
 def add_item(request):
     try:
-        cart_code = request.data.get('cart_code')
-        product_id = request.data.get('product_id')
+        cart_code = request.query_params.get('cart_code')
+        product_id = request.query_params.get('product_id')
 
         cart, created = Cart.objects.get_or_create(cart_code=cart_code)
         product = Product.objects.get(id=product_id)
@@ -35,3 +36,59 @@ def add_item(request):
         return Response({'data': serializer.data, 'message': 'Cartitem created successfully'}, status=201)
     except Exception as e:
         return Response({'error': str(e)}, status=400)
+
+
+@api_view(['GET'])
+def product_in_cart(request):
+    cart_code = request.query_params.get('cart_code')
+    product_id = request.query_params.get('product_id')
+
+    cart = Cart.objects.get(cart_code=cart_code)
+    product = Product.objects.get(id=product_id)
+
+    product_exists_in_cart = CartItem.objects.filter(cart=cart, product=product).exists()
+    return Response({'product_in_cart': product_exists_in_cart})
+
+
+@api_view(['GET'])
+def get_cart_stat(request):
+    cart_code = request.query_params.get('cart_code')
+    cart = Cart.objects.get(cart_code=cart_code)
+    serializer = SimpleCartSerializer(cart)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_cart(request):
+    cart_code = request.query_params.get('cart_code')
+    cart = Cart.objects.get(cart_code=cart_code, paid=False)
+    serializer = CartSerializer(cart)
+    return Response(serializer.data)
+
+
+@api_view(['PATCH'])
+def update_quantity(request):
+    try:
+        cartitem_id = request.data.get('item_id')
+        quantity = request.data.get('quantity')
+        cartitem = CartItem.objects.get(id=cartitem_id)
+        cartitem.quantity = quantity
+        cartitem.save()
+        serializer = CartItemSerializer
+        return Response({'data': serializer.data, 'message': 'Cartitem updated successfully!'}, status=200)
+    except Exception as e:
+        return Response({'error': str(e)}, status=400)
+    
+    
+@api_view(['DELETE'])
+def delete_cartitem(request):
+    try:
+        cartitem_id = request.data.get('item_id')
+        cartitem = CartItem.objects.get(id=cartitem_id)
+        cartitem.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    except Exception as e:
+        return Response({'error': str(e)}, status=400)
+
+
+
